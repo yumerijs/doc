0db5981893b4d4bd2fc720673d54563a41de6b8d
FireGuo <fireguo@flweb.cn>
2025-03-25 00:40:27 +0800

    更新 config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,25 +2,25 @@ export default {
     head: [
         ['link', { rel: 'icon', href: '/logo2.png' }],
     ],
-    title: 'MC WindyPear社区服',
+    title: 'Yumeri',
     lastUpdated: true,
-    description: 'MC WindyPear社区服，为打造良好的养老社区环境而努力',
+    description: '基于nodejs的新一代模块化web应用构建平台',
     lang: 'zh-CN',
     themeConfig: {
-        siteTitle: 'MC WindyPear社区服', //站点标题
+        siteTitle: 'Yumeri', //站点标题
         logo: "https://api.flweb.cn/logos/MCWP/icon.jpg",
         description: '一个Minecraft养老社区化服务器',//MCWindyPear Organization
         nav: [
             { text: "主页", link: "/" },
-            { text: "风梨网", link: "//www.flweb.cn" },
+            { text: "风梨团队", link: "//doc.flweb.cn" },
             { text: "鸣谢", link: "/structure/" },
         ],
         socialLinks: [
             { icon: "github", link: "https://github.com/windypear-team" },
         ],
         footer: {
-            message: 'MCWindyPear',
-            copyright: 'Copyright © 2018-2024 <a href="https://www.flweb.cn/">风梨团队</a>'
+            message: 'Yumerijs',
+            copyright: 'Copyright © 2018-2025 <a href="https://www.flweb.cn/">风梨团队</a>'
         },
         lastUpdatedText: "最近更新时间",
         search: {
