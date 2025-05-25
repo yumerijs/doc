83784113f101c2ae5fe167abb6e9e681a216cc54
FireGuo <fireguo@flweb.cn>
2025-05-25 10:34:07 +0800

    1

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,13 @@
 export default {
-    head: [
-        ['link', { rel: 'icon', href: 'https://api.flweb.cn/doc/image/bot.jpg' }],
-    ],
-    title: 'Yumeri',
-    lastUpdated: true,
-    description: '基于nodejs的新一代模块化web应用构建平台',
-    lang: 'zh-CN',
-    themeConfig: {
-        sidebar: {
+  head: [
+    ['link', { rel: 'icon', href: 'https://api.flweb.cn/doc/image/bot.jpg' }],
+  ],
+  title: 'Yumeri',
+  lastUpdated: true,
+  description: '基于nodejs的新一代模块化web应用构建平台',
+  lang: 'zh-CN',
+  themeConfig: {
+    sidebar: {
       '/start/': [
         {
           text: '开始',
@@ -24,15 +24,26 @@ export default {
           text: 'API参考',
           items: [
             { text: '总览', link: '/api/' },
-            { text: '插件 (Plugin)', link: '/api/plugin' },
-            { text: '指令 (Command)', link: '/api/command' },
-            { text: '事件 (Event)', link: '/api/event' },
-            { text: '中间件 (Middleware)', link: '/api/middleware' },
-            { text: '配置 (Config)', link: '/api/config' },
-            { text: '会话 (Session)', link: '/api/session' },
-            { text: '上下文 (Context)', link: '/api/context' },
-            { text : '平台 (Platform)', link: '/api/platform' },
-            { text: '日志 (Logger)', link: '/api/logger' }
+            {
+              text: '核心模块',
+              items: [
+                { text: '指令 (Command)', link: '/api/command' },
+                { text: '会话 (Session)', link: '/api/session' },
+                { text: '上下文 (Context)', link: '/api/context' },
+                { text: '配置 (Config)', link: '/api/config' },
+                { text: '平台 (Platform)', link: '/api/platform' },
+                { text: '日志 (Logger)', link: '/api/logger' }
+              ]
+            },
+            {
+              text: '组件',
+              items: [
+                { text: '事件 (Event)', link: '/api/event' },
+                { text: '中间件 (Middleware)', link: '/api/middleware' },
+                { text: '控制台 (Console)', link: '/api/console' },
+                { text: '数据库 (Database)', link: '/api/database' },
+              ]
+            }
           ]
         }
       ],
@@ -50,42 +61,42 @@ export default {
         }
       ]
     },
-        siteTitle: 'Yumeri', //站点标题
-        logo: "https://api.flweb.cn/doc/image/bot.jpg",
-        nav: [
-            { text: "主页", link: "/" },
-            { text: "开始", link: "/start/" },
-            { text: "开发指南", link: "/dev/" },
-            { text: "API参考", link: "/api/" },
-            { text: "风梨团队", link: "//doc.flweb.cn" },
-            { text: "鸣谢", link: "/structure/" },
-        ],
-        socialLinks: [
-            { icon: "github", link: "https://github.com/yumerijs" },
-        ],
-        footer: {
-            message: 'Yumerijs',
-            copyright: 'Copyright © 2018-2025 <a href="https://www.flweb.cn/">风梨团队</a>'
-        },
-        lastUpdatedText: "最近更新时间",
-        search: {
-            provider: "local",
-            options: {
-              translations: {
-                button: {
-                  buttonText: "搜索文章",
-                  buttonAriaLabel: "搜索文章",
-                },
-                modal: {
-                  noResultsText: "无法找到相关结果",
-                  resetButtonTitle: "清除查询条件",
-                  footer: {
-                    selectText: "选择",
-                    navigateText: "切换",
-                  },
-                },
-              },
+    siteTitle: 'Yumeri', //站点标题
+    logo: "https://api.flweb.cn/doc/image/bot.jpg",
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "开始", link: "/start/" },
+      { text: "开发指南", link: "/dev/" },
+      { text: "API参考", link: "/api/" },
+      { text: "风梨团队", link: "//doc.flweb.cn" },
+      { text: "鸣谢", link: "/structure/" },
+    ],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/yumerijs" },
+    ],
+    footer: {
+      message: 'Yumerijs',
+      copyright: 'Copyright © 2018-2025 <a href="https://doc.flweb.cn/">风梨团队</a>'
+    },
+    lastUpdatedText: "最近更新时间",
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文章",
+            buttonAriaLabel: "搜索文章",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
             },
           },
+        },
+      },
     },
+  },
 };
