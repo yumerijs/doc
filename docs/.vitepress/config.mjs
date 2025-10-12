497fdf71e708152f8123b73a60c22273655b2e4a
FireGuo1145 <fireguo@flweb.cn>
2025-10-13 00:44:57 +0800

    fix deadlink

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
     ['link', { rel: 'icon', href: 'https://api.flweb.cn/doc/image/bot.jpg' }],
   ],
   lastUpdated: true,
+  ignoreDeadLinks: true,
   locales: {
     root: {
       label: '简体中文',
