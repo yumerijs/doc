433ded87e3a6066ed21aa485a73d7d13e2306fe0
FireGuo <fireguo@flweb.cn>
2025-10-28 22:22:38 +0800

    更新 config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -65,6 +65,7 @@ export default defineConfig({
                     { text: '配置 (Config)', link: '/api/config' },
                     { text: '平台 (Platform)', link: '/api/platform' },
                     { text: '日志 (Logger)', link: '/api/logger' }
+                    { text: '国际化 (I18n)', link: '/api/i18n' }
                   ]
                 },
                 {
